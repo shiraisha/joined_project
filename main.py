089d2635c0c31bcb3222f3b89ca6a097a31a1abc
shoval45 <shoval29102004@gmail.com>
2022-09-18 12:12:24 +0300

    dog

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
 
+import Screen
 import consts
 
 state = {
     "state": consts.RUNNING_STATE,
-    "soldier_legs_location": consts.START_SOLDIER_LEGS_LOCATION
+    "soldier_legs_location": consts.START_SOLDIER_LEGS_LOCATION,
+    "is_window_open": True
 }
 
+def the_right_main():
+    pygame.init()
+    Screen.main()
+
+    while state["is_window_open"]
