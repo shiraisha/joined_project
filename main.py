824e3bd909ff6642b27811fc8d3b70112961d0d3
shoval45 <shoval29102004@gmail.com>
2022-09-18 15:42:44 +0300

    main

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ import consts
 state = {
     "state": consts.RUNNING_STATE,
     "soldier_legs_location": consts.START_SOLDIER_LEGS_LOCATION,
-    "is_window_open": True
+    "is_window_open": True,
+    "pressed_key_enter": False,
+    "pressed_key_up": False,
+    "pressed_key_down": False,
+    "pressed_key_left": False,
+    "pressed_key_Right": False
 }
 
-def the_right_main():
+def main():
     pygame.init()
-    Screen.main()
+    Screen.draw_game()
 
     while state["is_window_open"]:
         handle_user_events()
-
-
 
 def handle_user_events():
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             state["is_window_open"] = False
+
