cc8482c1d6578187891275e54c81e5cd7aab9076
shiraisha <shirnoa3@gmail.com>
2022-09-19 14:59:08 +0300

    update files

--- Soldier.py
+++ Soldier.py
@@ -17,30 +17,40 @@ def legs_location(game_state, pos):
     pos_legs_third_corner = [[24,0],[24,1]]
     pos_legs_fourth_corner = [[24,48],[24,49]]
     # check current pos and update to new pos
-    if pos != pos_legs_first_corner and pos != pos_legs_second_corner :
+    if compare_list(pos,pos_legs_first_corner) and compare_list(pos,pos_legs_second_corner):
         if game_state["pressed_key_up"]:
             pos[0][0] -= 1
             pos[1][0] -= 1
-    elif pos != pos_legs_first_corner and pos != pos_legs_third_corner:
+    elif compare_list(pos,pos_legs_first_corner) and compare_list(pos,pos_legs_third_corner):
         if game_state["pressed_key_left"]:
             pos[0][1] -= 1
             pos[1][1] -= 1
-    elif pos != pos_legs_third_corner and pos != pos_legs_fourth_corner:
+    elif compare_list(pos,pos_legs_third_corner) and compare_list(pos,pos_legs_fourth_corner):
         if game_state["pressed_key_down"]:
             pos[0][0] += 1
             pos[1][0] += 1
-    elif pos != pos_legs_second_corner and pos != pos_legs_fourth_corner:
+    elif compare_list(pos,pos_legs_second_corner) and compare_list(pos,pos_legs_fourth_corner):
         if game_state["pressed_key_right"]:
             pos[0][1] += 1
             pos[1][1] += 1
     return pos
+
+def compare_list(pos1, pos2):
+    check_list = True
+    for i in range(len(pos1)):
+        for j in range(len(pos1[i])):
+            if pos1[i][j] == pos2[i][j]:
+                check_list = True
+            else:
+                return False
+    return check_list
 
 
 def body_location(game_state,pos):
     initial_pos_body = [[0,0],[0,1],[1,0],[1,1],[2,0],[2,1]]
     second_pos_body = [[0,48],[0,49],[1,48],[1,49],[2,48],[2,49]]
     third_pos_body = [[21,0],[21,1],[22,0],[22,1],[23,0],[23,1]]
-    if pos != initial_pos_body and pos != second_pos_body:
+    if compare_list(pos,initial_pos_body) and compare_list(pos,second_pos_body):
         if game_state["pressed_key_up"]:
             pos[0][0] -= 1
             pos[1][0] -= 1
@@ -48,7 +58,7 @@ def body_location(game_state,pos):
             pos[3][0] -= 1
             pos[4][0] -= 1
             pos[5][0] -= 1
-    elif pos != third_pos_body:
+    elif compare_list(pos,third_pos_body):
         if game_state["pressed_key_down"]:
             pos[0][0] += 1
             pos[1][0] += 1
@@ -56,7 +66,7 @@ def body_location(game_state,pos):
             pos[3][0] += 1
             pos[4][0] += 1
             pos[5][0] += 1
-    elif pos != second_pos_body:
+    elif compare_list(pos,second_pos_body):
         if game_state["pressed_key_right"]:
             pos[0][1] += 1
             pos[1][1] += 1
@@ -64,7 +74,7 @@ def body_location(game_state,pos):
             pos[3][1] += 1
             pos[4][1] += 1
             pos[5][1] += 1
-    elif pos !=initial_pos_body and pos != third_pos_body:
+    elif compare_list(pos,initial_pos_body) and compare_list(pos,third_pos_body):
         if game_state["pressed_key_left"]:
             pos[0][1] -= 1
             pos[1][1] -= 1
