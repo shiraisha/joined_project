2e39d76e0e901183fe660eeb87c1681ed5fe201d
shoval45 <shoval29102004@gmail.com>
2022-09-19 10:01:54 +0300

    mainch

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def handle_user_events(player_soldier):
         elif state["state"] != consts.RUNNING_STATE:
             continue
 
-        if keys_pressed[pygame.KSCAN_KP_ENTER]:
+        if keys_pressed[pygame.K_RETURN]:
             state["pressed_key_enter"] = True
 
         else:
