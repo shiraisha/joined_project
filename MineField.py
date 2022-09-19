1ea08820349ec4f68263465b9a49ca6f6a823f10
shoval45 <shoval29102004@gmail.com>
2022-09-19 12:24:36 +0300

    mainch

--- MineField.py
+++ MineField.py
@@ -7,22 +7,22 @@ matrix_mine_pos = []
 
 def enter_flag_to_matrix():
     # entering flag indexes to matrix
-    for i in range(21, 24):
-        for j in range(46, 50):
+    for i in range(46, 50):
+        for j in range(21, 24):
             game_surface_matrix[i][j] = "flag"
     enter_start_player_location_to_matrix()
 
 def enter_start_player_location_to_matrix():
-    for i in range(0, 4):
-        for j in range(0, 2):
+    for i in range(0, 2):
+        for j in range(0, 4):
             game_surface_matrix[i][j] = "player_start"
     enter_mines()
 
 def enter_mines():
     for i in range(20):
-        position = [random.randint(0, 24), random.randint(0, 47)]
+        position = [random.randint(0, 47), random.randint(0, 24)]
         while check_if_three_indexes_are_not_empty(position):
-            position = [random.randint(0, 25), random.randint(0, 47)]
+            position = [random.randint(0, 47), random.randint(0, 24)]
         enter_mine_to_matrix(position, i)
         matrix_mine_pos.append(position)
 
